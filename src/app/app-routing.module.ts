c75e97dc2ad937f17d27f19562154fa399fce01a
Thiago Aramizo Ribeiro <aramizot@users.noreply.github.com>
2022-08-30 17:09:18 -0300

    Atualização de router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaItemComponent } from './lista-item/lista-item.component';
+import { NovoItemComponent } from './novo-item/novo-item.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'lista', pathMatch:'full'},
+  {path: 'lista', component: ListaItemComponent},
+  {path: 'novo-item', component: NovoItemComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
